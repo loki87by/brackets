d2c6d9753f437cd9849e593d6b617d3424cc8e4c
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-01 14:17:20 +0300

    feat: write needed function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('')
+  function cleaner(arr){
+    for(let i=0; i<arr.length; i++){
+      for(let j=0; j<bracketsConfig.length; j++){
+        if((arr[i] === bracketsConfig[j][0]) && (arr[i+1] === bracketsConfig[j][1])){
+          let start = []
+          if(i>0){start = arr.slice(0, i)}
+          let end = arr.slice(i+2)
+          let res = []
+          res.push(...start)
+          res.push(...end)
+          array = res
+        }
+      }
+    }
+  }
+  for(let x=array.length/2; x>0; x--){
+    cleaner(array)
+  }
+  //console.log(array)
+  if(array.length === 0){return true}
+  else{return false}
 }
